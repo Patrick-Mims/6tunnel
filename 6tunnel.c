4ba1350f6a655688bfa18510c4ca224121c2c703
patrick <patrickkevinmims@gmail.com>
2023-09-17 16:43:06 -0700

    Removed Comments

--- 6tunnel.c
+++ 6tunnel.c
@@ -1,26 +1,3 @@
-/*
- *  6tunnel v0.13
- *  (C) Copyright 2000-2005,2013,2016,2019 by Wojtek Kaniewski <wojtekka@toxygen.net>
- *
- *  Contributions by:
- *  - Dariusz Jackowski <ascent@linux.pl>
- *  - Ramunas Lukosevicius <lukoramu@parok.lt>
- *  - Roland Stigge <stigge@antcom.de>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License Version 2 as
- *  published by the Free Software Foundation.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
